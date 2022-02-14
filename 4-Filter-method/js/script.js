ff859fabe4b4db5a76f05785132dac0104280511
hreinn <hreinngy@gmail.com>
2022-02-14 20:40:11 +0100

    Fixing repo

--- 4-Filter-method/js/script.js
+++ 4-Filter-method/js/script.js
@@ -10,3 +10,31 @@
 // The filter method takes a function as an argument. This function receives each item in the array as an argument.
 // If the function returns true, the item is added to the new array.
 
+const numbers = [3, 4, 6, 77, 75];
+console.log("Before:", numbers);
+const filteredNumbers = numbers.filter(filterNumbers);
+
+function filterNumbers (numbers) {
+  if (numbers > 5) {
+    return true
+  }
+}
+
+console.log("After:", filteredNumbers);
+
+// Example 2
+// get even numbers
+
+const filteredNumbersTwo = numbers.filter((numbers) => {
+  if (numbers % 2 === 0) {
+    return true
+  }
+});
+
+/*function filterNumberTwo (numbers) {
+  if (numbers % 2 === 0) {
+    return true
+  }
+}*/
+
+console.log(filteredNumbersTwo)
